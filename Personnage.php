275fcae16b3df918fa14c4b63d27672be36a41b1
Krikod <kristen.kod@gmail.com>
2019-02-28 14:48:05 +0100

    visibilité des propr

--- Personnage.php
+++ Personnage.php
@@ -1,9 +1,35 @@
 <?php
 
 class Personnage {
-    public $vie = 21;
-    public $atk = 20;
-    public $nom;
+    // Par défaut, variables en PRIVATE. Pour accessibilité depuis ext., ->getters.
+    private $vie = 21; // public: accessible au sein de la classe par '$this->>prop'
+    // et du dehors $merlin->nom
+    // PRIVATE que dans la classe -> ou fct getNom(), return $this->>nom
+    // accès au dehors: $merlin->getNom();
+    // PROTECTED: (moins strict que private) accès depuis d'autres classes qui hériteraient de celle-ci.
+    private $atk = 20;
+    private $nom;
+    // Meth. pour inverse, setter $this->>nom = $nom
+
+    public function getVie()
+    {
+        return $this->vie;
+    }
+
+    public function getAtk()
+    {
+        return $this->atk;
+    }
+
+    public function getNom()
+    {
+        return "M. " . $this->nom;
+    }
+
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
 
     public function __construct($nom)
     {
--- index.php
+++ index.php
@@ -12,13 +12,14 @@ $harry = new Personnage("Harry");
 //$harry->vie = 0;
 
 $merlin->attaque($harry);
+$merlin->setNom('JUJu');
 
 if ($harry->mort()) {
     echo "Harry est mort";
 } else {
-    echo "Harry a survécu. Il lui reste " . $harry->vie . " points de vie";
+    echo "Harry a survécu. Il lui reste " . $harry->getVie() . " points de vie";
 }
 //echo '<pre>';
-var_dump($merlin);
-var_dump($harry);
+var_dump($merlin->getNom());
+var_dump($harry->getAtk());
 //echo '</pre>';
