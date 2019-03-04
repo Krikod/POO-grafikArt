443f51d99d69a29d3e3211bc191e03a13e6663da
Krikod <kristen.kod@gmail.com>
2019-03-04 19:28:39 +0100

    class documentation

--- form.php
+++ form.php
@@ -1,22 +1,47 @@
 <?php
 
+/**
+ * Class Form
+ * Allows to generate a form easily
+ */
+
 class Form
 {
+    /**
+     * @var array Data used by the form
+     */
     private $data;
+
+    /**
+     * @var string Tag used to surround fields
+     */
     public $surround = 'p';
 
+
+    /**
+     * Form constructor.
+     * @param array $data Data used by the form
+     */
     public function __construct($data = array())
         // $data array, par défaut vide, donc pas obligé en param ds index
     {
         $this->data = $data;
     }
 
+    /**
+     * @param $html string Surrounded HTML code
+     * @return string
+     */
     private function surround($html)
     {
         return "<{$this->surround}>{$html}</$this->surround}>";
         // accolades pour que la variable soi interprétée par les ""
     }
 
+    /**
+     * @param $index string Index of the value to get
+     * @return mixed|null
+     */
     private function getValue($index)
     {
         return isset($this->data[$index]) ? $this->data[$index]: null ;
@@ -29,6 +54,11 @@ class Form
 //        );
 //    }
 
+    /**
+     * @param $label string
+     * @param $type string
+     * @param $name string
+     */
     public function input($label, $type, $name)
     {
         echo '<p><label for="'.$name.'" />'.$label.'</label></p>
@@ -36,6 +66,9 @@ class Form
             value="'.$this->getValue($name).'" /></p>';
     }
 
+    /**
+     * @return string
+     */
     public function submit()
     {
         return $this->surround('<button type="submit">Envoyer</button>');
--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $form = new Form($_POST);
 ?>
 <form action="#" method="post">
     <?php
-    echo $form->input('Nom:', 'text', 'username');
+    echo $form->input('Nom: ', 'text', 'username');
     echo $form->input('Mot de passe: ', 'password', 'password');
     echo $form->submit();
     ?>
